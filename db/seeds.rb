33d65bf940bf187db0b82e89949d743e81546ac7
Annie <anniehwang428@gmail.com>
2023-01-26 14:54:58 -0500

    add model with seeds for initial data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Tweet.create(text: "my name is barry allen and i am the fastest man alive", user_id: 1)
+
+Tweet.create(text: "big man in a suit of armour, take that away, what are you", user_id: 2)
+
+Tweet.create(text: "genius, billionaire, playboy, philantrhopist", user_id: 3)
